ebe09939f802f2887ffe4f0758cff0deb092bc56
Felipe <felipesantosdd@gmail.com>
2023-06-15 04:56:14 -0300

    update: Alteração no nome do card baixado, agora o nome do card será o titulo do card

--- pages/index.vue
+++ pages/index.vue
@@ -1121,9 +1121,10 @@ export default {
         const blob = canvas.msToBlob()
         window.navigator.msSaveBlob(blob, 'YuGiOh.png')
       } else {
+        const data = this.cardMeta[this.cardLang].Default
         const a = document.createElement('a')
         a.href = canvas.toDataURL('image/jpeg')
-        a.download = 'YuGiOh.jpg'
+        a.download = `${data.title}.jpg`
         a.click()
       }
     },
